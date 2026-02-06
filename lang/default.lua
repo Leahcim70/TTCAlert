e4ab1a03f07ed9639bc5d0e5b3edb7ef3ef57b7b
Michael Lantzen <michael@lantzen.com>
2026-02-07 00:07:48 +0100

    LibPrice statt TTC

--- lang/default.lua
+++ lang/default.lua
@@ -1,9 +1,9 @@
 -- Default (English) strings for TTCLootAlert
-ZO_CreateStringId("TTCLootAlert_LOOT_MESSAGE", "|cFFD700[LP]|r %s: Price %sg (x%d = %sg)")
-ZO_CreateStringId("TTCLootAlert_THRESHOLD_CURRENT", "|cFFD700[LP]|r Current threshold: %sg. Use /ttcalert <gold> to set.")
-ZO_CreateStringId("TTCLootAlert_INVALID_VALUE", "|cFFD700[LP]|r Invalid value. Use /ttcalert <gold>.")
-ZO_CreateStringId("TTCLootAlert_THRESHOLD_SET", "|cFFD700[LP]|r Threshold set to %sg.")
-ZO_CreateStringId("TTCLootAlert_ADDON_LOADED", "|cFFD700[LP]|r %s loaded (threshold: %sg).")
+ZO_CreateStringId("TTCLootAlert_LOOT_MESSAGE", "|cFFD700[LA]|r %s: Price %sg (x%d = %sg)")
+ZO_CreateStringId("TTCLootAlert_THRESHOLD_CURRENT", "|cFFD700[LA]|r Current threshold: %sg. Use /ttcalert <gold> to set.")
+ZO_CreateStringId("TTCLootAlert_INVALID_VALUE", "|cFFD700[LA]|r Invalid value. Use /ttcalert <gold>.")
+ZO_CreateStringId("TTCLootAlert_THRESHOLD_SET", "|cFFD700[LA]|r Threshold set to %sg.")
+ZO_CreateStringId("TTCLootAlert_ADDON_LOADED", "|cFFD700[LA]|r %s loaded (threshold: %sg).")
 
 SafeAddVersion(TTCLootAlert_LOOT_MESSAGE, 1)
 SafeAddVersion(TTCLootAlert_THRESHOLD_CURRENT, 1)
